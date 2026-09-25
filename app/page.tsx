cc44763aea0484cca182acc3131ab9e6f97bd7ee
Ahmad Rofi'us Su'ud <ahmadrofiussuud@gmail.com>
2026-09-25 20:51:31 +0700

    Fix FooterSection import in page.tsx and confirm clean build

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import LoveStorySection from "@/components/LoveStorySection";
 import GallerySection from "@/components/GallerySection";
 import RsvpSection from "@/components/RsvpSection";
 import DigitalGiftSection from "@/components/DigitalGiftSection";
+import FooterSection from "@/components/FooterSection";
 import { FallingPetalsOverlay } from "@/components/FallingPetalsOverlay";
 
 function InvitationContent() {
